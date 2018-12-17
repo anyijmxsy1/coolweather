853303deffc6736c5389aed427c3b59aebdfecfc
anyijmxsy1 <jmxsy@21cn.com>
2018-12-17 18:18:19 +0800

    加入数据库遍历功能能

---
+++ app/src/main/java/com/samon/coolweather/ChooseAreaFragment.java
@@ -0,0 +1,166 @@
+package com.samon.coolweather;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.samon.coolweather.db.City;
+import com.samon.coolweather.db.County;
+import com.samon.coolweather.db.Province;
+import com.samon.coolweather.util.HttpUtil;
+import com.samon.coolweather.util.Utility;
+
+import org.litepal.crud.DataSupport;
+import org.litepal.util.BaseUtility;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class ChooseAreaFragment extends Fragment {
+
+    public static final int LEVEL_PROVINCE = 0;
+    public static final int LEVEL_CITY = 1;
+    public static final int LEVEL_COUNTY = 2;
+
+
+    private TextView titleText;
+    private Button backButton;
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private List<String> dataList = new ArrayList<>();
+    
+    private List<Province> provinceList;
+    private List<City> cityList;
+    private List<County> countyList;
+    
+    private Province selectedProvince;
+    private City selectedCity;
+    private int currentLevel;
+
+    /*
+    第一步：将碎片布局中的控件实例化，并为listview设置适配器
+     */
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.choose_area,container,false);
+        titleText = view.findViewById(R.id.title_text);
+        backButton = view.findViewById(R.id.back_button);
+        listView = view.findViewById(R.id.list_view);
+        adapter = new ArrayAdapter<>(getContext(),android.R.layout.simple_list_item_1,dataList);
+        listView.setAdapter(adapter);
+        return view;
+    }
+
+    
+    /*
+    第二步：在该方法内，先进行点击事件的判断，若是第一次进入，先执行queryProvinces()方法。
+     */
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(currentLevel == LEVEL_PROVINCE){
+                    selectedProvince = provinceList.get(position);
+                    queryCities();
+                }else if (currentLevel == LEVEL_CITY){
+                    selectedCity = cityList.get(position);
+                    queryCountis();
+                }
+            }
+        });
+        queryProvinces();
+    }
+
+    private void queryCountis() {
+        titleText.setText("中国");
+        backButton.setVisibility(View.GONE);
+        provinceList = DataSupport.findAll(Province.class);
+        if (provinceList.size()>0){
+            dataList.clear();
+            for (Province province:provinceList){
+                dataList.add(province.getProvinceName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel = LEVEL_PROVINCE;
+        }else {
+            String address = "http://guolin.tech/api/china";
+            queryFromServer(address,"province");
+        }
+    }
+
+    /*
+    根据传入的地址和类型从数据加查询省市县数据
+     */
+    private void queryFromServer(String address, final String type) {
+        HttpUtil.sendOkHttpRequest(address, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getContext(), "加载失败", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String responseText = response.body().string();
+                boolean result = false;
+                if ("province".equals(type)){
+                    result =Utility.handleProvinceResponse(responseText);
+                }else if ("city".equals(type)){
+                    result = Utility.handleCityResponse(responseText,selectedProvince.getId());
+                }else if ("county".equals(type)){
+                    result = Utility.handleCountyResponse(responseText,selectedCity.getId());
+                }
+
+                if (result){
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if ("province".equals(type)){
+                                queryProvinces();
+                            }else if ("city".equals(type)){
+                                queryCities();
+                            }else if ("county".equals(type)){
+                                queryCountis();
+                            }
+                        }
+                    });
+                }
+
+            }
+
+        });
+    }
+
+    private void queryCities() {
+    }
+
+    private void queryProvinces(){
+
+    }
+
+
+}
